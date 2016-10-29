6e72e2d8b13abe76d3c002f7a6488c826fe920e9
tuyennt6628-massgenie <tuyennt6628@massgenie.com>
2016-10-29 10:32:19 +0700

    1

---
+++ 03. SourceCode/DAL/AnhThayDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+namespace DAL
+{
+    public class AnhThayDbContext
+    {
+        private IDbConnection _connection;
+        private static AnhThayDbContext _context;
+
+        public AnhThayDbContext()
+        {
+
+        }
+
+        public static AnhThayDbContext Context
+        {
+            get
+            {
+                if(_context == null)
+                {
+                    return new AnhThayDbContext();
+                }
+
+                return _context;
+            }
+        }
+
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                if(_connection == null)
+                {
+                    _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["AnhThayDbContext"].ConnectionString);
+                }
+                if(_connection.State != ConnectionState.Open)
+                {
+                    _connection.Open();
+                }
+
+                return _connection;
+            }
+        }
+
+        public void Dispose()
+        {
+            if(_connection != null && _connection.State == ConnectionState.Open)
+                _connection.Close();
+        }
+    }
+}
