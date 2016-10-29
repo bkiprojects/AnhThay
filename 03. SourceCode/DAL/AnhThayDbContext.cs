61d6ce35f0b87f51893d5b9ba13875aa9df01de7
Thanh Tuyen Nguyen <Thanh Tuyen Nguyen>
2016-10-29 11:57:15 +0700

    Changed connection string

--- 03. SourceCode/DAL/AnhThayDbContext.cs
+++ 03. SourceCode/DAL/AnhThayDbContext.cs
@@ -35,7 +35,7 @@ namespace DAL
             {
                 if(_connection == null)
                 {
-                    _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["AnhThayDbContext"].ConnectionString);
+                    _connection = new SqlConnection(getConnection());
                 }
                 if(_connection.State != ConnectionState.Open)
                 {
@@ -46,10 +46,34 @@ namespace DAL
             }
         }
 
+        
+
         public void Dispose()
         {
             if(_connection != null && _connection.State == ConnectionState.Open)
                 _connection.Close();
+        }
+
+        private string getConnection()
+        {
+            var server = ConfigurationManager.AppSettings["SERVER"];
+            var initial_database = ConfigurationManager.AppSettings["INITIAL_DATABASE"]; 
+            var fill_database = ConfigurationManager.AppSettings["FILL_DATABASE"];
+            var initial_user = ConfigurationManager.AppSettings["INITIAL_USER"];
+            var pass_word = ConfigurationManager.AppSettings["PASS_WORD"];
+
+            var connectionString = string.Format("Data Source={1};initial catalog={2};"
+                                        +"Integrated Security={3};"
+                                        +"User Id={4};Password={5};"
+                                        +"MultipleActiveResultSets={6}",
+                                        server,
+                                        initial_database,
+                                         false,
+                                         initial_user,
+                                         pass_word,
+                                         true);
+
+            return connectionString;
         }
     }
 }
